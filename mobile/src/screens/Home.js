0d131c138dcaade5171ad48634a979728f034f83
Nitish Sainani <37032957+nitishsainani@users.noreply.github.com>
2021-08-12 14:56:45 +0530

    Update Home.js

--- mobile/src/screens/Home.js
+++ mobile/src/screens/Home.js
@@ -60,18 +60,6 @@ function Home(props) {
   useEffect(() => {
     AsyncStorage.getItem('asdf').then(console.log);
 
-    // GoogleSignin.configure({
-    //   scopes: ['profile', 'email',], // what API you want to access on behalf of the user, default is email and profile
-    //   webClientId: '185816397363-gkbjq2leic2q8sr9i9gd9279001nc79t.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
-    //   offlineAccess: false, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-    //   // hostedDomain: '', // specifies a hosted domain restriction
-    //   // loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
-    //   // forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
-    //   // accountName: '', // [Android] specifies an account name on the device that should be used
-    //   iosClientId: '185816397363-kg3s84pnt1td68qp923pgup4jfqdbfa4.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
-    //   // googleServicePlistPath: '', // [iOS] optional, if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
-    // });
-
     setPage(1);
     RecipeService
       .getRecipes(1, size)
@@ -101,12 +89,6 @@ function Home(props) {
   return (<>
     <Block flex center style={styles.home}>
       <Block style={styles.articles}>
-        {/*<GoogleSigninButton*/}
-        {/*  style={{ width: 192, height: 48 }}*/}
-        {/*  size={GoogleSigninButton.Size.Wide}*/}
-        {/*  color={GoogleSigninButton.Color.Dark}*/}
-        {/*  onPress={_signIn}*/}
-        {/*  disabled={false} />*/}
         <RecipeList recipes={recipes} onEndReached={onEndReached} />
       </Block>
     </Block>
